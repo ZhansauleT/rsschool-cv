5ce22847e70c088868e1a4c797827c9be43a2282
Zhansaule Telisheva <jansaule97@mail.ru>
2022-12-17 21:01:36 +0500

    feat: add menu button functionality

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let menuButton = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu");
+let menuItem = document.querySelector(".menu__item");
+
+const menuPressed = e => {
+  menu.classList.toggle('menu--visible');
+}
+
+menu.addEventListener("click", menuPressed);
+
+menuButton.addEventListener('click', function () {
+  menu.classList.toggle('menu--visible');
+});
+
